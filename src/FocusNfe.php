b0ba402cf01dace4a57d6ed5d9b8bff54679f9aa
Matheus Macrineu <macrineu.developer@gmail.com>
2025-03-18 11:29:37 -0300

    feat: add chamada de trait

--- src/FocusNfe.php
+++ src/FocusNfe.php
@@ -1,13 +1,10 @@
 <?php
 
+namespace Macrineeu\SdkFocusnfe;
+
+use Macrineeu\SdkFocusnfe\Traits\ServiceTrait;
+
 class FocusNfe
 {
-    private $isProduction = false;
-    private $token;
-
-    public function __construct(bool $isProduction, string $token)
-    {
-        $this->isProduction = $isProduction;
-        $this->token = $token;
-    }
+    use ServiceTrait;
 }
