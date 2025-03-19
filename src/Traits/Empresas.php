b1ee86d12aea04c15a8d738cdb7d6487c4348b06
Matheus Macrineu <macrineu.developer@gmail.com>
2025-03-19 00:24:51 -0300

    feat: corrige validacao de dados enviados

--- src/Traits/Empresas.php
+++ src/Traits/Empresas.php
@@ -12,7 +12,7 @@ class Empresas
         $this->isProduction = $isProduction;
     }
 
-    public function create($data): array
+    public function create(array $data): array
     {
         $requiredFields = (new EmpresaRequest())->create($data);
 
@@ -25,37 +25,37 @@ class Empresas
             'nome_fantasia' => $data['nome_fantasia'],
             'cnpj' => str_replace(['.','-','/'], '', $data['cnpj']),
             'inscricao_estadual' => str_replace(['.','-','/'], '', $data['inscricao_estadual']),
-            'inscricao_municipal' => str_replace(['.','-','/'], '', $data['inscricao_municipal']),
+            'inscricao_municipal' => isset($data['inscricao_municipal']) && !empty($data['inscricao_municipal']) ? str_replace(['.','-','/'], '', $data['inscricao_municipal']) : null,
             'regime_tributario' => $data['regime_tributario'],
             'email' => $data['email'],
             'telefone' => str_replace(['(', ')', '-', ' ', '_'], '', $data['telefone']),
             'logradouro' => $data['logradouro'],
             'numero' => $data['numero'],
-            'complemento' => $data['complemento'],
+            'complemento' => $data['complemento'] ?? null,
             'bairro' => $data['bairro'],
-            'cep' => str_replace(['-'], '', $data['cep']),
+            'cep' => str_replace(['-', ' ', '.'], '', $data['cep']),
             'municipio' => $data['municipio'],
             'uf' => $data['uf'],
-            'enviar_email_destinatario' => false,
-            'descrimina_impostos' => true,
-            'habilita_nfe' => true,
-            'habilita_nfce' => true,
-            'mostrar_danfse_badge' => false,
-            'enviar_email_homologacao' => false,
-            'csc_nfce_producao' => $data['csc_nfce_producao'],
-            'csc_nfce_homologacao' => $data['csc_nfce_homologacao'],
-            'id_token_nfce_producao' => $data['id_token_nfce_producao'],
-            'id_token_nfce_homologacao' => $data['id_token_nfce_homologacao'],
-            'proximo_numero_nfe_producao' => $data['proximo_numero_nfe_producao'],
-            'proximo_numero_nfe_homologacao' => $data['proximo_numero_nfe_homologacao'],
-            'serie_nfe_producao' => $data['serie_nfe_producao'],
-            'serie_nfe_homologacao' => $data['serie_nfe_homologacao'],
-            'proximo_numero_nfce_producao' => $data['proximo_numero_nfce_producao'],
-            'proximo_numero_nfce_homologacao' => $data['proximo_numero_nfce_homologacao'],
-            'serie_nfce_producao' => $data['serie_nfce_producao'],
-            'serie_nfce_homologacao' => $data['serie_nfce_homologacao'],
+            'enviar_email_destinatario' => $data['enviar_email_destinatario'] ?? false,
+            'descrimina_impostos' => $data['descrimina_impostos'] ?? false,
+            'habilita_nfe' => $data['habilita_nfe'] ?? false,
+            'habilita_nfce' => $data['habilita_nfce'] ?? false,
+            'mostrar_danfse_badge' => $data['mostra_danfse_badge'] ?? false,
+            'enviar_email_homologacao' => $data['enviar_email_homologacao'] ?? false,
+            'csc_nfce_producao' => $data['csc_nfce_producao'] ?? null,
+            'csc_nfce_homologacao' => $data['csc_nfce_homologacao'] ?? null,
+            'id_token_nfce_producao' => $data['id_token_nfce_producao'] ?? null,
+            'id_token_nfce_homologacao' => $data['id_token_nfce_homologacao'] ?? null,
+            'proximo_numero_nfe_producao' => $data['proximo_numero_nfe_producao'] ?? null,
+            'proximo_numero_nfe_homologacao' => $data['proximo_numero_nfe_homologacao'] ?? null,
+            'serie_nfe_producao' => $data['serie_nfe_producao'] ?? null,
+            'serie_nfe_homologacao' => $data['serie_nfe_homologacao'] ?? null,
+            'proximo_numero_nfce_producao' => $data['proximo_numero_nfce_producao'] ?? null,
+            'proximo_numero_nfce_homologacao' => $data['proximo_numero_nfce_homologacao'] ?? null,
+            'serie_nfce_producao' => $data['serie_nfce_producao'] ?? null,
+            'serie_nfce_homologacao' => $data['serie_nfce_homologacao'] ?? null,
         ];
 
-        return ['production' => $this->isProduction, 'data' => $body];
+        return ['production' => (bool) $this->isProduction, 'data' => $body];
     }
 }
